bd23aa7e6a047bf438e540623cdf4c04993eea22
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-06 19:22:37 +0530

    digital_clock_App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const ttime= new Date().toLocaleTimeString();
+  const [time,setTime]=useState(ttime);
   const getTime = () =>{
+    console.log(time);
     console.log('you click')
   }
   return (
     <>
-    <h1>time</h1>
+    <h1>{time}</h1>
     <button onClick={getTime}>Get Time  t</button>
     </>
   );

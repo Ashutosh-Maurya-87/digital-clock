ea98399f08ec78fac0bfe7842cbba7f668561f8e
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-06 18:12:53 +0530

    digital_clock_App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>time</h1>
+    <button>Get Time</button>
+    </>
+  );
+}
+
+export default App;

f355c541f5fd7e831a1e4feba4bc42728a155d12
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-06 19:16:46 +0530

    digital_clock_App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getTime = () =>{
+    console.log('you click')
+  }
   return (
     <>
     <h1>time</h1>
-    <button>Get Time</button>
+    <button onClick={getTime}>Get Time  t</button>
     </>
   );
 }
